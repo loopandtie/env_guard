8b5671719b94ebbebb9adbed3f3301f2c1cd9622
Preston Mueller <preston.mueller@me.com>
2018-07-10 16:36:43 -0400

    Fix for rails 5.2, use ActiveSupport::SecurityUtils.variable_size_secure_compare when available (<5.2) and ActiveSupport::SecurityUtils.secure_compare otherwise

--- lib/env_guard.rb
+++ lib/env_guard.rb
@@ -19,9 +19,12 @@ module EnvGuard
         raise ArgumentError, "Must set the ENV variables ENVGUARD_USERNAME and ENVGUARD_PASSWORD"
       end
 
+      available_secure_compare = ActiveSupport::SecurityUtils.respond_to?(:variable_size_secure_compare) ?
+        :variable_size_secure_compare : :secure_compare
+
       authenticate_or_request_with_http_basic("Application") do |name, password|
-        granted = ActiveSupport::SecurityUtils.variable_size_secure_compare(name, (ENV['ENVGUARD_USERNAME'].presence)) &
-          ActiveSupport::SecurityUtils.variable_size_secure_compare(password, (ENV['ENVGUARD_PASSWORD'].presence))
+        granted = ActiveSupport::SecurityUtils.send(available_secure_compare, name, (ENV['ENVGUARD_USERNAME'].presence)) &
+          ActiveSupport::SecurityUtils.send(available_secure_compare, password, (ENV['ENVGUARD_PASSWORD'].presence))
         session[:authenticated_for_environment_guard] = granted
         granted
       end
